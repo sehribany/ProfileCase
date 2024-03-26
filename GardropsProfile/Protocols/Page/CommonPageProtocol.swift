a8130dea6f6ee7ed870469359714c2a9a502fcdd
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-03-27 01:16:34 +0300

    CommonPageProtocl

---
+++ GardropsProfile/Protocols/Page/CommonPageProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  CommonPageProtocol.swift
+//  GardropsProfile
+//
+//  Created by Şehriban Yıldırım on 23.03.2024.
+//
+
+protocol CommonPageProtocol {
+    func scrollingDidEnd(index: Int)
+}
