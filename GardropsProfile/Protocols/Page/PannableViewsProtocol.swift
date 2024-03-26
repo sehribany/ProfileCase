5ac30068c40286b7a79ddd3fd631871e0fcab97c
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-03-27 01:16:58 +0300

    PannableViewsProtocol

---
+++ GardropsProfile/Protocols/Page/PannableViewsProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  PannableViewsProtocol.swift
+//  GardropsProfile
+//
+//  Created by Şehriban Yıldırım on 22.03.2024.
+//
+
+import UIKit
+
+public protocol PannableViewsProtocol {
+    func panView() -> UIView
+}
