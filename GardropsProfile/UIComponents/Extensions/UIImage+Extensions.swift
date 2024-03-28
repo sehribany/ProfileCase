ccc93f78157f1825fd9f2c3782015e55e622f156
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-03-29 02:17:03 +0300

    UIImage+Extensions Created.

---
+++ GardropsProfile/UIComponents/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIImage+Extensions.swift
+//  GardropsProfile
+//
+//  Created by Şehriban Yıldırım on 25.03.2024.
+//
+
+import UIKit
+
+extension UIImage {
+    convenience init(color: UIColor, size: CGSize) {
+        UIGraphicsBeginImageContextWithOptions(size, false, 1)
+        color.set()
+        let ctx = UIGraphicsGetCurrentContext()!
+        ctx.fill(CGRect(origin: .zero, size: size))
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        self.init(data: image.pngData()!)!
+    }
+}
